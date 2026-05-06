51e3785e01bf61cc7396da1be03674ea12e4c941
Gooses <emquillerier@gmail.com>
2026-05-06 14:43:41 +0200

    initial commit

---
+++ backend/notSpot/src/main/java/com/goose/notspot/NotSpotApplication.java
@@ -0,0 +1,13 @@
+package com.goose.notspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotSpotApplication.class, args);
+    }
+
+}
