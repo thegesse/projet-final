9205f78346e07a3fef872ec4b0c9aab602d652b3
Gooses <emquillerier@gmail.com>
2026-05-12 17:32:28 +0200

    started on model layer of mobile frontend

---
+++ frontend_mobile/not_spot/lib/models/dto/song_dto.dart
@@ -0,0 +1,13 @@
+class SongDto {
+  final int id;
+  final String title;
+  final String artist;
+  final String streamUrl;
+
+  const SongDTO({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.streamUrl,
+  });
+}
