02bd90c0f76769d6f5f6231863d57885dcb4b29b
Gooses <emquillerier@gmail.com>
2026-05-19 09:52:29 +0200

    added song api

---
+++ frontend_mobile/not_spot/lib/features/songs/data/song_api.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+
+import '../../../core/config/app_config.dart';
+import '../../../core/network/api_client.dart';
+import '../models/domain/song.dart';
+import '../models/dto/song_dto.dart';
+import '../models/requests/add_song_request.dart';
+
+class SongApi {
+  Future<List<Song>> getSongs() async {
+    final response = await ApiClient.get(AppConfig.songsUri());
+
+    return (response as List)
+        .map((json) => Song.fromDTO(SongDTO.fromJson(json)))
+        .toList();
+  }
+
+  Future<Song> getSong(int songId) async {
+    final response = await ApiClient.get(AppConfig.songUri(songId));
+    return Song.fromDTO(SongDTO.fromJson(response));
+  }
+
+  Future<List<Song>> searchSong(String query) async {
+    final response = await ApiClient.get(AppConfig.searchSongsUri(query));
+
+    return (response as List)
+        .map((json) => Song.fromDTO(SongDTO.fromJson(json)))
+        .toList();
+  }
+
+  Future<Song> addSong(AddSongRequest request, File songFile) async {
+    final dio = Dio();
+
+    final formData = FormData.fromMap({
+      'title': request.title,
+      'artist': request.artist,
+      'file': await MultipartFile.fromFile(songFile.path,
+          filename: songFile.path.split('/').last),
+    });
+
+    final response = await dio.post(AppConfig.songsUri().toString(),
+        data: formData,
+        options: Options(headers: {'Accept': 'Application/json'}));
+
+    return Song.fromDTO(SongDTO.fromJson(response.data));
+  }
+
+  Future<void> deleteSong(int songId) async {
+    await ApiClient.delete(AppConfig.songUri(songId));
+  }
+}
