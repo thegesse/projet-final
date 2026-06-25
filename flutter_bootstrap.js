eabbb69f8e7069c7b2b7268bd576ac1f96a55d3d
thegesse <goose.minipainting@gmail.com>
2026-06-25 22:40:22 +0200

    Update production web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3729239654"
+    serviceWorkerVersion: "2282449649"
   }
 });
